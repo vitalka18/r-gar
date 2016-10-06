f350f81bdb00a17551b4918e81d4fe375bf1cb9e
vitalka181094 <vitalka18@i.ua>
2016-10-06 14:18:27 +0300

    end

---
+++ build/js/common.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+  mainSlider();
+
+  $('#top-go').click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({scrollTop:0},800);
+  });
+
+  setEqualHeight($('.base-info-card__img-title'));
+});
+
+ymaps.ready(function () {
+  if ( $('#map').length ) {
+    var myMap = new ymaps.Map('map', {
+      center: [59.889708, 30.478156],
+      zoom: 14,
+      controls: []
+    }),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map.png',
+      iconImageSize: [195, 184],
+      iconImageOffset: [-90, -180]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable('scrollZoom');
+  }
+  
+  if ( $('#mapContact').length ) {
+    var myContactMap = new ymaps.Map('mapContact', {
+      center: [59.889708, 30.478156],
+      zoom: 5,
+      controls: ['zoomControl', 'typeSelector']
+    });
+
+    var contactStore = [
+      {x: 59.889708, y: 30.478156}
+    ];
+
+    contactStore.forEach(function(store) {
+      var myPlacemark = new ymaps.Placemark([store.x, store.y], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-2.png',
+        iconImageSize: [44, 40],
+        iconImageOffset: [-22, -22]
+      });
+
+      myContactMap.geoObjects.add(myPlacemark);
+      myContactMap.behaviors.disable('scrollZoom');
+    });
+  }
+
+  $('#lightboxModal').on('show.bs.modal', function (e) {
+    var $link = $(e.relatedTarget);
+    var $modal = $(e.target);
+    var $img = $modal.find('img');
+    $img.attr({src: $link.attr('data-img')});
+  });
+});
+
+
+
+
+
+function mainSlider() {
+  $('.js-about-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+}
+
+function setEqualHeight(columns){
+  var tallestcolumn = 0;
+  columns.each(
+  function(){
+    currentHeight = $(this).height();
+    if(currentHeight > tallestcolumn){
+      tallestcolumn = currentHeight;
+    }
+  });
+  columns.height(tallestcolumn);
+}
